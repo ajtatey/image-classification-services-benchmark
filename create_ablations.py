59d32d9f1764c21556714b8ed07038c1b1d59afc
Andrew Tate <tate.andrewjames@gmail.com>
2022-12-22 21:26:56 -0500

    Create create_ablations.py

---
+++ create_ablations.py
@@ -0,0 +1,178 @@
+import csv
+import os
+import random
+import shutil
+
+training_list_filename = 'chest_xray_train.csv'
+testing_list_filename = 'chest_xray_test.csv'
+google_bucket_name = 'gs://chest-xray'
+# get the class names from the training folder
+CLASSES = os.listdir('train')
+# save the class names to a file
+CLASSES.remove('.DS_Store')
+with open('classes.txt', 'w') as f:
+    f.write(','.join(CLASSES))
+# create a training list from all the files in the training folder
+training_list = []
+for cls in CLASSES:
+    training_list.extend([file, cls] for file in os.listdir(f'train/{cls}'))
+
+# shuffle the training list
+random.shuffle(training_list)
+# write the training list to a csv
+with open(training_list_filename, 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(training_list)
+
+testing_list = []
+for cls in CLASSES:
+    testing_list.extend([file, cls] for file in os.listdir(f'test/{cls}'))
+
+with open(testing_list_filename, 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(testing_list)
+
+ABLATIONS = [1280, 320, 80, 20, 5]
+
+class_list = [[], []]
+train_list = [[], []]
+val_list = [[], []]
+for cls in CLASSES:
+    ablation_count = 0
+    class_list = [[], []]
+    vertex_train_list = [[], []]
+    vertex_val_list = [[], []]
+    hg_train_list = [['file'], ['label']]
+    hg_val_list = [['file'], ['label']]
+    nyckel_train_list = [[], []]
+    with open(training_list_filename) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[1] == cls and ablation_count < ABLATIONS[0]:
+                class_list[0].append(row[0])
+                class_list[1].append(cls)
+                ablation_count += 1
+    train_list[0].extend(class_list[0][:int(ABLATIONS[0]*0.8)])
+    train_list[1].extend(class_list[1][:int(ABLATIONS[0]*0.8)])
+    for file_name, file_class in zip(train_list[0], train_list[1]):
+        vertex_train_list[0].append(
+            f'{google_bucket_name}/training_uploads/{file_name}')
+        vertex_train_list[1].append(file_class)
+    hg_train_list[0].extend(train_list[0])
+    hg_train_list[1].extend(train_list[1])
+    # concatenate the validation list and the training list into nyckel_train_list
+    nyckel_train_list[0].extend(train_list[0])
+    nyckel_train_list[1].extend(train_list[1])
+
+    val_list[0].extend(class_list[0][int(ABLATIONS[0]*0.8):])
+    val_list[1].extend(class_list[1][int(ABLATIONS[0]*0.8):])
+    for file_name, file_class in zip(val_list[0], val_list[1]):
+        vertex_val_list[0].append(
+            f'{google_bucket_name}/val_uploads/{file_name}')
+        vertex_val_list[1].append(file_class)
+    hg_val_list[0].extend(val_list[0])
+    hg_val_list[1].extend(val_list[1])
+    nyckel_train_list[0].extend(val_list[0])
+    nyckel_train_list[1].extend(val_list[1])
+
+
+with open(f'train_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*train_list))
+with open(f'train_vertex_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*vertex_train_list))
+with open(f'train_hg_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*hg_train_list))
+with open(f'train_nyckel_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*nyckel_train_list))
+with open(f'val_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*val_list))
+with open(f'val_vertex_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*vertex_val_list))
+with open(f'val_hg_{ABLATIONS[0]}.csv', 'w') as f:
+    a = csv.writer(f, delimiter=',')
+    a.writerows(zip(*hg_val_list))
+
+# copy all the files listed in the training list to a new hg_training folder
+# create hg_training folder if it doesn't exist
+if not os.path.exists('training_uploads'):
+    os.makedirs('training_uploads')
+
+for file_name, file_class in zip(train_list[0], train_list[1]):
+    shutil.copyfile(f'train/{file_class}/{file_name}',
+                    f'training_uploads/{file_name}')
+if not os.path.exists('val_uploads'):
+    os.makedirs('val_uploads')
+for file_name, file_class in zip(val_list[0], val_list[1]):
+    shutil.copyfile(f'train/{file_class}/{file_name}',
+                    f'val_uploads/{file_name}')
+
+for ablation in ABLATIONS[1:]:
+    temp_train_list = [[], []]
+    vertex_list = [[], []]
+    hg_list = [['file'], ['label']]
+    nyckel_train_list = [[], []]
+
+    for cls in CLASSES:
+        ablation_count = 0
+        for file_name, file_class in zip(train_list[0], train_list[1]):
+            if file_class == cls and ablation_count < int(ablation*0.8):
+                temp_train_list[0].append(file_name)
+                temp_train_list[1].append(cls)
+                ablation_count += 1
+    train_list = temp_train_list
+    for file_name, file_class in zip(train_list[0], train_list[1]):
+        vertex_list[0].append(
+            f'{google_bucket_name}/training_uploads/{file_name}')
+        vertex_list[1].append(file_class)
+    hg_train_list[0].extend(train_list[0])
+    hg_train_list[1].extend(train_list[1])
+    nyckel_train_list[0].extend(train_list[0])
+    nyckel_train_list[1].extend(train_list[1])
+    with open(f'train_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*train_list))
+    with open(f'train_vertex_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*vertex_list))
+    with open(f'train_hg_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*hg_train_list))
+
+    temp_val_list = [[], []]
+    vertex_list = [[], []]
+    hg_list = [['file'], ['label']]
+    for cls in CLASSES:
+        ablation_count = 0
+        for file_name, file_class in zip(val_list[0], val_list[1]):
+            if file_class == cls and ablation_count < ablation*0.2:
+                temp_val_list[0].append(file_name)
+                temp_val_list[1].append(cls)
+                ablation_count += 1
+    val_list = temp_val_list
+    for file_name, file_class in zip(val_list[0], val_list[1]):
+        vertex_list[0].append(
+            f'{google_bucket_name}/val_uploads/{file_name}')
+        vertex_list[1].append(file_class)
+    hg_val_list[0].extend(val_list[0])
+    hg_val_list[1].extend(val_list[1])
+    nyckel_train_list[0].extend(val_list[0])
+    nyckel_train_list[1].extend(val_list[1])
+
+    with open(f'val_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*val_list))
+    with open(f'val_vertex_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*vertex_list))
+    with open(f'val_hg_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*hg_val_list))
+    with open(f'train_nyckel_{ablation}.csv', 'w') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(zip(*nyckel_train_list))
